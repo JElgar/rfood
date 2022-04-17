295d4884eddc330fd170c9abcea07e5f18dc15ef
jelgar <jamesnelgar@gmail.com>
2022-04-17 19:01:47 +0100

    Add outputs to git

---
+++ outputs/src/set/fp.rs
@@ -0,0 +1,81 @@
+pub trait Set {
+    fn union(self: Box<Self>, right: Box<dyn Set>) -> Box<dyn Set>;
+    fn is_empty(&self) -> bool;
+    fn contains(&self, target: i32) -> bool;
+    fn insert(self: Box<Self>, value: i32) -> Box<dyn Set>;
+}
+pub struct Empty {}
+impl Set for Empty {
+    fn union(self: Box<Self>, right: Box<dyn Set>) -> Box<dyn Set> {
+        right
+    }
+    fn is_empty(&self) -> bool {
+        true
+    }
+    fn contains(&self, target: i32) -> bool {
+        false
+    }
+    fn insert(self: Box<Self>, value: i32) -> Box<dyn Set> {
+        if self.contains(value) {
+            return self;
+        }
+        return Box::new(Insert { s1: self, value });
+    }
+}
+pub struct Insert {
+    pub s1: Box<dyn Set>,
+    pub value: i32,
+}
+impl Set for Insert {
+    fn union(self: Box<Self>, right: Box<dyn Set>) -> Box<dyn Set> {
+        Box::new(Union {
+            s1: self,
+            s2: right,
+        })
+    }
+    fn is_empty(&self) -> bool {
+        false
+    }
+    fn contains(&self, target: i32) -> bool {
+        self.value == target || self.s1.contains(target)
+    }
+    fn insert(self: Box<Self>, value: i32) -> Box<dyn Set> {
+        if self.contains(self.value) {
+            return self;
+        }
+        return Box::new(Insert { s1: self, value });
+    }
+}
+pub struct Union {
+    pub s1: Box<dyn Set>,
+    pub s2: Box<dyn Set>,
+}
+impl Set for Union {
+    fn union(self: Box<Self>, right: Box<dyn Set>) -> Box<dyn Set> {
+        Box::new(Union {
+            s1: self,
+            s2: right,
+        })
+    }
+    fn is_empty(&self) -> bool {
+        self.s1.is_empty() && self.s2.is_empty()
+    }
+    fn contains(&self, target: i32) -> bool {
+        self.s1.contains(target) || self.s2.contains(target)
+    }
+    fn insert(self: Box<Self>, value: i32) -> Box<dyn Set> {
+        if self.contains(value) {
+            return self;
+        }
+        return Box::new(Insert { s1: self, value });
+    }
+}
+pub fn demo() {
+    let empty = Box::new(Empty {});
+    let set = empty.insert(1);
+    let set2 = Box::new(Insert {
+        s1: Box::new(Empty {}),
+        value: 1,
+    });
+    let _set3 = set.union(set2);
+}
---
+++ outputs/tests/test_outputs.rs
@@ -0,0 +1,42 @@
+#[test]
+fn test_output_set_oop() {
+    use outputs::set::oop::*;
+    
+    let empty = Box::new(Set::Empty {});
+    let set = insert(*empty, 1);
+
+    let set2 = Box::new(Set::Insert {
+        set1: Box::new(Set::Empty {}),
+        value: 1,
+    });
+
+    let set3 = union(set, *set2);
+
+    assert!(contains(&set3, 1));
+    assert!(!contains(&set3, 4));
+
+    let set3 = insert(set3, 2);
+    assert!(contains(&set3, 2));
+}
+
+#[test]
+fn test_output_set_fp() {
+    use outputs::set::fp::*;
+
+    let empty = Box::new(Empty{});
+    let set = empty.insert(1);
+
+    let set2 = Box::new(Insert {
+        s1: Box::new(Empty{}),
+        value: 4
+    });
+
+    let set3 = set.union(set2);
+
+    assert!(set3.contains(1));
+    assert!(set3.contains(4));
+    assert!(!set3.contains(2));
+
+    let set3 = set3.insert(2);
+    assert!(set3.contains(2));
+}
