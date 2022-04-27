faf2e3274580b59fc4464044c366cb17258a6cfd
James Elgar <jamesnelgar@gmail.com>
2022-04-27 13:59:20 +0100

    Update output of oop stage 1 snipper

--- report/snippets/type_def_transfom_oop.rs
+++ report/snippets/type_def_transfom_oop.rs
@@ -1,19 +1,11 @@
 pub trait Exp {
-    // ...
 }
 
 pub struct Lit{
     pub n: i32,
 }
-impl Exp for Lit {
-    // ...
-}
 
 pub struct Sub {
     pub l: Box<dyn Exp>,
     pub r: Box<dyn Exp>,
-}
-
-impl Exp for Sub {
-    // ...
 }
