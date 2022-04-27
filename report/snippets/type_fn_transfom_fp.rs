399981d82dcc23396ab0f390841da9ddb0cc8814
James Elgar <jamesnelgar@gmail.com>
2022-04-27 14:22:03 +0100

    Update output snippets for stage 2

--- report/snippets/type_fn_transfom_fp.rs
+++ report/snippets/type_fn_transfom_fp.rs
@@ -1,18 +1,22 @@
-enum Exp {
-    Lit { n: i32 },
-    Sub { l: Box<Exp>, r: Box<Exp> },
+trait Exp {
+    fn eval(&self) -> i32;
 }
-fn eval(exp: &Exp) -> i32 {
-    return match exp {
-        Exp::Lit { n } => *n,
-        Exp::Sub { l, r } => eval(l) - eval(r),
-    };
+
+struct Lit {
+    pub n: i32,
 }
-pub fn demo() {
-    let exp = Box::new(Exp::Sub {
-        l: Box::new(Exp::Lit { n: 1 }),
-        r: Box::new(Exp::Lit { n: 2 }),
-    });
-    let out = eval(&exp);
-    print!("{}", out);
+impl Exp for Lit {
+    fn eval(&self) -> i32 {
+        self.n
+    }
+}
+
+struct Sub {
+    pub l: Box<dyn Exp>,
+    pub r: Box<dyn Exp>,
+}
+impl Exp for Sub {
+    fn eval(&self) -> i32 {
+        self.l.eval() - self.r.eval()
+    }
 }
--- report/snippets/type_fn_transfom_oop.rs
+++ report/snippets/type_fn_transfom_oop.rs
@@ -2,6 +2,7 @@ pub enum Exp {
     Lit { n: i32 },
     Sub { l: Box<Exp>, r: Box<Exp> },
 }
+
 pub fn eval(exp: &Exp) -> i32 {
     match &exp {
         Exp::Lit { n } => {
@@ -11,12 +12,4 @@ pub fn eval(exp: &Exp) -> i32 {
             return l.eval() - r.eval();
         }
     }
-}
-pub fn demo() {
-    let e = Box::new(Sub {
-        l: Box::new(Lit { n: 2 }),
-        r: Box::new(Lit { n: 1 }),
-    });
-    let _result = e.eval();
-    println!("{}", e.eval());
 }
